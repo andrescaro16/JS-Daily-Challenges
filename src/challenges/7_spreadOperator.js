b8d55f10248de99ea5d45efca098d8cb882e40d2
andrescaro16 <julicaro2003@gmail.com>
2023-03-03 13:44:01 -0500

    Spread operator
    Permite expandir iterables en elementos individuales

---
+++ src/challenges/7_spreadOperator.js
@@ -0,0 +1,10 @@
+// ¿Qué imprime este código?
+
+console.log([...'Oscar']);
+
+
+
+/* Un string es un elemento iterable en JavaScript, por ende es posible usar el spread operator 
+directamente obteniendo la propagación de la cadena letra por letra. 
+
+RESULTADO: ["O", "s", "c", "a", "r"] */
