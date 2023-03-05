ba1406b19f8e245c94d85d2cb8943809bc574268
andrescaro16 <julicaro2003@gmail.com>
2023-03-04 23:15:13 -0500

    Método: setInterval
    Permite imprimir el cuerpo de una función en un determinado intervalo

---
+++ src/challenges/8_setInterval.js
@@ -0,0 +1,9 @@
+// ¿Qué imprime este código?
+
+setInterval(() => console.log('Hi'), 1000);
+
+
+/* La función setInterval es una Web Api que recibe un intervalo en milisegundos, 
+e imprime el cuerpo de la función en dicho intervalo. 
+
+RESULTADO: Hi cada segundo*/
