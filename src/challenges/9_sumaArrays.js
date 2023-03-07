ea6d0fed81b8badc23233a4ccb84141b411de10b
andrescaro16 <julicaro2003@gmail.com>
2023-03-07 09:51:36 -0500

    Asignacion

--- src/challenges/9_sumaArrays.js
+++ src/challenges/9_sumaArrays.js
@@ -11,39 +11,3 @@ como esto aunque no lo veamos:
 console.log(typeof ([].toString() + [].toString())); //string console.log(typeof ("" + "")); console.log(typeof ("")); //string 
 
 RESULTADO: string*/
-
-
-
-
-function getStudentAverage(students) {
-  const averageStudent = students.map(function(student) {
-    const subjectGrades = student.grades.reduce(function(acumulador, num) {
-      return (acumulador + num);
-    }, 0);
-
-    const temp = [];
-
-    for (const num of student.grades) {
-      temp.push(num / 4);
-    }
-
-    return temp;
-  });
-
-  console.log(averageStudent);
-}
-
-getStudentAverage([
-  {
-    name: "Pedro",
-    grades: [90, 87, 88, 90],
-  },
-  {
-    name: "Jose",
-    grades: [99, 71, 88, 96],
-  },
-  {
-    name: "Maria",
-    grades: [92, 81, 80, 96],
-  },
-]);
