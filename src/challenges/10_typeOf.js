e04ea14e826743ebc724da4fcafb3a79847535dc
andrescaro16 <julicaro2003@gmail.com>
2023-03-06 21:21:52 -0500

    typeof

---
+++ src/challenges/10_typeOf.js
@@ -0,0 +1,10 @@
+// ¿Qué imprime este código?
+
+console.log(typeof typeof 1);
+
+
+
+/* Esta pregunta es un poco trampa. Pero la respuesta es chistosa: typeof 1 regresa "number", 
+literalemnte la cadena "number", entonces tendriamos typeof "number" y esto da obiamente string. 
+
+RESPUESTA: string*/
