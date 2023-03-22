42542768f9749df37f9c29f26250851ceed07880
andrescaro16 <julicaro2003@gmail.com>
2023-03-22 08:46:50 -0500

    Function class
    Arrow function limitation

---
+++ src/challenges/25_functionClass.js
@@ -0,0 +1,23 @@
+// ¿Qué imprimirá este código?
+
+let Alimento = function (comida) {
+  this.comida = comida;
+};
+
+Alimento.prototype.comerHoy = () => console.log('Hoy toca comer:', this.comida);
+const alimento = new Alimento('ensalada');
+
+alimento.comerHoy();
+
+
+
+/* 
+Una de las limitaciones de las funciones flecha (arrow functions), es que no tiene sus propias referencias 
+a this o super y no se debe usar como métodos.
+
+this siempre apuntará a this desde el contexto en el que definimos la función (ejemplo window).
+
+Por tal razón, la función flecha () => { ... } no sabe de this.comida
+
+RESULTADO: Hoy toca comer: undefined
+*/
