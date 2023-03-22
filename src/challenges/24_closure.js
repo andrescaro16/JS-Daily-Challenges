2ff61a3cfe0699d1e2dbb34450843ae9de2f3507
andrescaro16 <julicaro2003@gmail.com>
2023-03-21 22:04:00 -0500

    Closure

---
+++ src/challenges/24_closure.js
@@ -0,0 +1,29 @@
+// ¿Qué imprime este código?
+
+function makeMulti(x) {
+  return (y) => x * y;
+}
+
+const five = makeMulti(5);
+const rta = five(6);
+console.log(rta);
+
+
+
+/* 
+Aquí tenemos el escenario de un closure, un closure es:
+
+- Una función que puede recibir un dato y lo almacena en su scope
+- Retorna una función que ocupa ese dato previamente guardado
+
+En este caso estamos haciendo una "fábrica" de multiplicadores.
+
+// five es una función que tiene guardada en su scope la variable x con el valor de 5
+const five = makeMulti(5);
+
+// Ahora podemos pasarle a la función five cualquier valor que queramos multiplicar por 5
+const rta = five(6);
+
+
+RESPUESTA: 30
+*/
