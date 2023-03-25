89c4be060e95f9184ae672ef37838724fc408956
andrescaro16 <julicaro2003@gmail.com>
2023-03-25 10:22:05 -0500

    map Exercise

---
+++ src/challenges/28_map_Exercise.js
@@ -0,0 +1,15 @@
+// ¿Qué hace este código?
+
+const array = ['a', 'bb', 'ccc'];
+const rta = array.map((item) => item.length);
+
+
+
+/* 
+El método map() crea un nuevo array con los resultados de la llamada a la función indicada aplicados a cada uno de sus elementos.
+
+.map() es inmutable, por lo tanto, no modifica el array original, sino que crea uno nuevo con la “transformación” aplicada. Además, 
+mantienes el mismo length que el array original, te devuelve en el nuevo array la misma cantidad que el array que le aplicaste el método.
+
+RESULTADO: [ 1, 2, 3 ]
+*/
