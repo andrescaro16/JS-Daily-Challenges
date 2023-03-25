527b0575e894bd6427072673ad57cb071cd75270
andrescaro16 <julicaro2003@gmail.com>
2023-03-24 21:39:26 -0500

    Throw y try y catch

---
+++ src/challenges/27_throw.js
@@ -0,0 +1,28 @@
+// ¿Qué se imprimirá?
+
+function greeting() {
+  throw 'Hello world!';
+}
+
+function sayHi() {
+  try {
+    const data = greeting();
+    console.log('It worked!', data);
+  } catch (e) {
+    console.log('Oh no an error!', e);
+  }
+}
+
+sayHi();
+
+
+
+/* 
+La función greeting con la palabra reservada throw genera una excepción de tipo string en el código.
+
+La función sayHi consta de una sentencia try...catch, recordemos que si no hay ningún tipo de excepción el código 
+ejecuta el bloque try pero como si generamos una excepción entonces entramos al bloque catch donde el parámetro e 
+adopta el valor de la excepción, osea, Hello world!.
+
+RESULTADO: "Oh no an error! Hello world!"
+*/
